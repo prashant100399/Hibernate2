f2a496bf4a2154669e2551d7d34083f1ea4850f8
prashant100399 <prashantavipatil@gmail.com>
2019-12-27 11:58:41 +0530

    addding new method in show data class

--- Hibernet/src/com/avi/show/ShowData.java
+++ Hibernet/src/com/avi/show/ShowData.java
@@ -3,7 +3,14 @@ package com.avi.show;
 public class ShowData {
 
 	
+	public void newMethosAdded() {
+		
+		System.out.println("New method added successfullt");
+	}
+	
 	public void showDataMethod() {
 		System.out.println("in show class method ");
+		
+		
 	}
 }
